887b868615bc7377f37fd0bf17b7afb270237f94
infog2 dev <roney@infog2.com.br>
2016-05-12 18:50:08 -0300

    Recibo ligue taxi

---
+++ LigueTaxi/SaoLuiz/uRelRecibo.pas
@@ -0,0 +1,109 @@
+unit uRelRecibo;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, ExtCtrls, QuickRpt, QRCtrls;
+
+type
+  TfrmRelRecibo = class(TForm)
+    qrRecibo: TQuickRep;
+    TitleBand1: TQRBand;
+    QRImage1: TQRImage;
+    QRLabel1: TQRLabel;
+    QRMemo1: TQRMemo;
+    QRShape1: TQRShape;
+    QRShape2: TQRShape;
+    QRShape30: TQRShape;
+    QRLabel2: TQRLabel;
+    QRShape31: TQRShape;
+    QRShape4: TQRShape;
+    QRShape3: TQRShape;
+    QRMemo3: TQRMemo;
+    QRImage2: TQRImage;
+    QRMemo2: TQRMemo;
+    QRLabel3: TQRLabel;
+    QRLabel4: TQRLabel;
+    QRShape5: TQRShape;
+    QRShape6: TQRShape;
+    QRShape7: TQRShape;
+    QRShape8: TQRShape;
+    QRShape9: TQRShape;
+    QRMemo4: TQRMemo;
+    txtNMRECIBO: TQRLabel;
+    QRLabel6: TQRLabel;
+    txtRecebemos: TQRLabel;
+    QRLabel8: TQRLabel;
+    txtValor: TQRLabel;
+    QRShape10: TQRShape;
+    QRShape11: TQRShape;
+    QRShape12: TQRShape;
+    QRLabel10: TQRLabel;
+    txtImportancia: TQRLabel;
+    QRShape13: TQRShape;
+    QRLabel12: TQRLabel;
+    txtReferente: TQRMemo;
+    QRShape14: TQRShape;
+    QRShape15: TQRShape;
+    QRLabel13: TQRLabel;
+    txtObs: TQRMemo;
+    QRShape28: TQRShape;
+    QRShape29: TQRShape;
+    QRLabel14: TQRLabel;
+    txtDia: TQRLabel;
+    QRLabel18: TQRLabel;
+    txtMes: TQRLabel;
+    QRLabel24: TQRLabel;
+    txtAno: TQRLabel;
+    QRShape32: TQRShape;
+    QRShape33: TQRShape;
+    QRShape34: TQRShape;
+    QRShape35: TQRShape;
+    QRLabel28: TQRLabel;
+    txtRecibo2: TQRLabel;
+    QRLabel17: TQRLabel;
+    txtRecebemos2: TQRLabel;
+    QRShape27: TQRShape;
+    QRLabel15: TQRLabel;
+    txtVALOR2: TQRLabel;
+    QRShape16: TQRShape;
+    QRShape26: TQRShape;
+    QRLabel19: TQRLabel;
+    txtImportancia2: TQRLabel;
+    QRShape24: TQRShape;
+    QRLabel21: TQRLabel;
+    txtReferentes2: TQRMemo;
+    QRShape23: TQRShape;
+    QRShape25: TQRShape;
+    txtObs2: TQRMemo;
+    QRLabel22: TQRLabel;
+    QRShape17: TQRShape;
+    QRShape22: TQRShape;
+    txtDia2: TQRLabel;
+    QRLabel23: TQRLabel;
+    QRShape21: TQRShape;
+    QRLabel29: TQRLabel;
+    QRShape18: TQRShape;
+    QRLabel25: TQRLabel;
+    txtMes2: TQRLabel;
+    QRShape19: TQRShape;
+    QRLabel27: TQRLabel;
+    txtAno2: TQRLabel;
+    QRShape20: TQRShape;
+    QRShape36: TQRShape;
+    QRShape37: TQRShape;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmRelRecibo: TfrmRelRecibo;
+
+implementation
+
+{$R *.dfm}
+
+end.
---
+++ LigueTaxi/SaoLuiz/uRelRecido2.pas
@@ -0,0 +1,59 @@
+unit uRelRecido2;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, ExtCtrls, QuickRpt, QRCtrls;
+
+type
+  TfrmRelRecibo2 = class(TForm)
+    qrRelatorio: TQuickRep;
+    TitleBand1: TQRBand;
+    QRShape1: TQRShape;
+    QRImage1: TQRImage;
+    QRShape3: TQRShape;
+    QRShape4: TQRShape;
+    txtTexto: TQRMemo;
+    QRLabel2: TQRLabel;
+    QRLabel3: TQRLabel;
+    QRShape5: TQRShape;
+    txtData: TQRLabel;
+    QRShape6: TQRShape;
+    QRShape7: TQRShape;
+    QRShape8: TQRShape;
+    QRImage2: TQRImage;
+    QRShape10: TQRShape;
+    QRShape11: TQRShape;
+    txtTexto2: TQRMemo;
+    QRLabel6: TQRLabel;
+    QRLabel7: TQRLabel;
+    QRShape12: TQRShape;
+    txtData2: TQRLabel;
+    QRShape13: TQRShape;
+    QRShape14: TQRShape;
+    QRShape15: TQRShape;
+    QRMemo1: TQRMemo;
+    QRLabel1: TQRLabel;
+    QRLabel4: TQRLabel;
+    QRLabel5: TQRLabel;
+    QRLabel8: TQRLabel;
+    QRMemo2: TQRMemo;
+    QRShape16: TQRShape;
+    QRMemo3: TQRMemo;
+    QRMemo4: TQRMemo;
+    QRShape2: TQRShape;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmRelRecibo2: TfrmRelRecibo2;
+
+implementation
+
+{$R *.dfm}
+
+end.
